af5b86c0f62a13817057de233ef5f567e69a4f7f
kiomnd2 <kiomnd2@naver.com>
2022-01-03 15:41:13 +0900

    init

---
+++ src/main/java/kiomnd2/cosmo/CosmoApiApplication.java
@@ -0,0 +1,13 @@
+package kiomnd2.cosmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kiomnd2/cosmo/CosmoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kiomnd2.cosmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
